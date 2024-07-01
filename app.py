e872be2c44ad92962cb3b7b49dc0bb998ea6b4a9
chandima2000 <hychandima2000@gmail.com>
2024-07-01 16:15:17 +0530

    create routes for home and predict html pages

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from flask import Flask, request, jsonify, send_file, render_template
+import re
 
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+import pandas as pd
+import pickle
+
+STOPWORDS = set(stopwords.words("english"))
 
 app= Flask(__name__)
 
@@ -9,6 +16,16 @@ app= Flask(__name__)
 def test():
     return "Test request received successfully. Service is running."
 
+
+@app.route("/",methods=["GET"])
+def home():
+    return render_template("index.html")
+
+
+
+@app.route("/predict",methods=["GET"])
+def predict():
+    return render_template("predict.html")
 
 
 if __name__ == "__main__":
